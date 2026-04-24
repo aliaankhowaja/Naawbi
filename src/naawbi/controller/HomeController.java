7f36ceabb718af02601b638af13229bfc635aa15
ibbi1020 <ibraheemfrq@gmail.com>
2026-04-24 15:04:48 +0500

    feat: add HomeView FXML and CSS for course grid

---
+++ src/naawbi/controller/HomeController.java
@@ -0,0 +1,10 @@
+package naawbi.controller;
+
+import javafx.fxml.Initializable;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HomeController implements Initializable {
+    @Override
+    public void initialize(URL l, ResourceBundle r) {}
+}
